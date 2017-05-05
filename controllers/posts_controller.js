7de86257d6e1a0a018c7bbe7f80458b85a19d906
Be-Smart <serhii.vladymyrov@gmail.com>
2017-05-05 16:08:11 +0300

    Update, create, delete

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,9 +1,10 @@
 const BlogPost = require('../models/blogPost');
+const moment = require('moment');
 
 module.exports = {
 
   allPosts(req, res) {
-    BlogPost.find({})
+    BlogPost.find({}).sort({createdAt: -1})
       .then(posts => {
         res.render('posts-list', { title: 'My Blog', posts });
       });
@@ -14,18 +15,51 @@ module.exports = {
 
     BlogPost.findOne({_id: postId})
       .then(post => {
-        res.render('post', post);
+        const { title, content } = post;
+        const createdAt = moment(post.createdAt).format('MMM, Do, YYYY');
+
+        res.render('post', {title, content, createdAt, postId});
       })
   },
 
-  create(req, res) {
-    const postProp = req.body;
-
-    BlogPost.create(postProp)
-      .then(post => res.send(post))
-      .catch(err => console.log(err));
+  newPost(req, res) {
+    res.render('new-post', {title: 'Create new blog post'});
   },
 
-  edit(req, res) {},
-  remove(req, res) {}
+  create(req, res) {
+    BlogPost.create(req.body)
+      .then(() => {
+        res.redirect('/');
+      })
+  },
+
+  editPage(req, res) {
+    const postId = req.params.id;
+
+    BlogPost.findOne({_id: postId})
+      .then(post => {
+        const { title, content } = post;
+        res.render('edit-post', {title, content, postId});
+      })
+  },
+
+  edit(req, res) {
+    const postId = req.params.id;
+    const { title, content } = req.body;
+    const updatedAt = new Date();
+
+    BlogPost.findOneAndUpdate({_id: postId}, {title, content, updatedAt})
+      .then(() => {
+        res.redirect(`/post/${postId}`);
+      });
+  },
+
+  remove(req, res) {
+    const postId = req.params.id;
+
+    BlogPost.findOneAndRemove({_id: postId})
+      .then(() => {
+        res.redirect('/');
+      })
+  }
 };
--- routes.js
+++ routes.js
@@ -4,8 +4,9 @@ module.exports = (app) => {
   app.get('/', PostsController.allPosts);
 
   app.get('/post/:id', PostsController.getPost);
-  app.put('/post/:id', PostsController.edit);
-  app.delete('/post/:id', PostsController.remove);
-
-  app.post('/post/new', PostsController.create);
+  app.get('/post/create/new', PostsController.newPost);
+  app.post('/post/create/new', PostsController.create);
+  app.get('/post/edit/:id', PostsController.editPage);
+  app.post('/post/edit/:id', PostsController.edit);
+  app.get('/post/delete/:id', PostsController.remove);
 };
