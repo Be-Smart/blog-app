b55b37cd6d26d2976e04fe4dd1367432973fcaa2
Be-Smart <serhii.vladymyrov@gmail.com>
2017-06-04 22:51:34 +0300

    Base sass files structure

--- client/main.js
+++ client/main.js
@@ -1,4 +1,16 @@
-import './main.sass';
+import './sass/main.sass';
+
+// const getElement = domElement => document.querySelector(domElement);
+//
+// const toggleClass = (domElement, className) => () => {
+//   getElement(domElement).classList.toggle(className);
+// };
+//
+// const attachListener = (eventType, elementToListen, callback) => {
+//   getElement(elementToListen).addEventListener(eventType, () => callback());
+// };
+//
+// attachListener('click', 'button', toggleClass('body', 'bg'));
 
 if (module.hot) {
   module.hot.accept();
